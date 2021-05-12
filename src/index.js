15800d6a7b970eae816d6d8dd6e8c232ae9d47bf
tkielarbb <tomek.kielar00@gmail.com>
2021-05-12 19:35:34 +0200

    Added first solution to the problem Due to an issue with API , other (Countries) API has been used

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
 
-ReactDOM.render(
-  <React.StrictMode>
+const API_KEY = '25f50ab3-2426-447f-a4dc-a521a96ec52e'
+
+const client = new ApolloClient({
+  uri: `https://api.everbase.co/graphql?apikey=${API_KEY}`,
+  cache: new InMemoryCache(),
+});
+
+
+ReactDOM.render( 
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
