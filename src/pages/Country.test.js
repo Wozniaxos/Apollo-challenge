2490abe3c5eeb4a0071454aed8999d8d8b16af7c
Tomasz Kielar <tomek.kielar00@gmail.com>
2021-05-21 14:24:21 +0200

    fix: apollo query now works in Country page
    changed id type to string in countryQuery

--- src/pages/Country.test.js
+++ src/pages/Country.test.js
@@ -1,8 +1,8 @@
 import { MockedProvider } from "@apollo/client/testing";
 import Country from "./Country";
-import COUNTRY_QUERY from '../queries/countryQuery'
-import { MemoryRouter } from "react-router-dom";
-import { render,waitFor,cleanup } from "@testing-library/react";
+import COUNTRY_QUERY from "../queries/countryQuery";
+import { MemoryRouter, Route } from "react-router-dom";
+import { render, waitFor, cleanup } from "@testing-library/react";
 
 const mocks = [
   {
@@ -19,56 +19,72 @@ const mocks = [
             name: "Afghanistan",
             vatRate: null,
             id: "Q889",
+            capital: {
+              id: "Q5838",
+              name: "Kabul",
+            },
             cities: [
               {
+                id: "Q183303",
                 name: "Jalalabad",
               },
               {
-                name: "Sheberghan",
-              },
-              {
-                name: "Sar-e Pol",
-              },
-              {
-                name: "Mazar-i-Sharif",
-              },
-              {
-                name: "Khost",
-              },
-              {
-                name: "Khan Abad",
-              },
-              {
-                name: "Taloqan",
-              },
-              {
-                name: "Lashkar Gah",
-              },
-              {
-                name: "Kandahar",
-              },
-              {
+                id: "Q732879",
                 name: "Baghlan",
               },
               {
+                id: "Q476800",
+                name: "Lashkar Gah",
+              },
+              {
+                id: "Q1649301",
+                name: "Sar-e Pol",
+              },
+              {
+                id: "Q838954",
+                name: "Sheberghan",
+              },
+              {
+                id: "Q130469",
+                name: "Mazar-i-Sharif",
+              },
+              {
+                id: "Q386682",
+                name: "Khost",
+              },
+              {
+                id: "Q1247205",
+                name: "Khan Abad",
+              },
+              {
+                id: "Q45604",
+                name: "Kandahar",
+              },
+              {
+                id: "Q477238",
+                name: "Taloqan",
+              },
+              {
+                id: "Q1014375",
                 name: "Kholm",
               },
               {
+                id: "Q5838",
                 name: "Kabul",
               },
               {
+                id: "Q173731",
                 name: "Ghazni",
               },
               {
+                id: "Q477148",
                 name: "Pul-e Khomri",
               },
               {
+                id: "Q45313",
                 name: "Herat",
               },
             ],
-            capital: {
-              name: "Kabul",
-            },
           },
         ],
       },
@@ -76,26 +92,24 @@ const mocks = [
   },
 ];
 
-
-  const componentToRender = () => render(
-    <MemoryRouter initialEntries={['/country/Q889']}>
-        <MockedProvider mocks={mocks} addTypename={false}>
-          <Country match={{params: {
-              countryId: "Q889"
-          }}} />
-        </MockedProvider>
+const componentToRender = ({ countryId }) =>
+  render(
+    <MemoryRouter initialEntries={[`/countries/${countryId}`]}>
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Route path="/countries/:id">
+          <Country match={{ params: { id: countryId } }} />
+        </Route>
+      </MockedProvider>
     </MemoryRouter>
   );
 
-  afterEach(cleanup)
-  it("Properly renders country loading state", async () => {
-    const { getByText } = componentToRender();
-    await waitFor(() => getByText(/Loading.../i));
-  });
+afterEach(cleanup);
+it("Properly renders country loading state", async () => {
+  const { getByText } = componentToRender({ countryId: "Q889" });
+  await waitFor(() => getByText(/Loading.../i));
+});
 
-  it("Properly renders country data", async () => {
-    const { getByText } = componentToRender();
-    await waitFor(() => getByText(/Afghanistan.../i));
-  });
-
-
+it("Properly renders country data", async () => {
+  const { getByText } = componentToRender({ countryId: "Q889" });
+  await waitFor(() => getByText(/Afghanistan/i));
+});
